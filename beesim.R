c5bf4cf41c76e7885881cbf24906529acd1277e1
Samson Su <michellewang@Samson-Sus-MacBook-Pro.local>
2015-06-03 21:11:37 -0700

    r file

---
+++ beesim.R
@@ -0,0 +1,40 @@
+bee = read.table("beedata.csv", sep=",", header=TRUE)
+hist(bee$Transit.Time, breaks=seq(0, 21, by=2), prob = TRUE, main = "Histogram of Estimated Transit Times",
+     xlab = "Estimated transit time (mo per 100 km)", ylab = "Frequency")
+
+# lines(density(bee$Transit.Time), lwd = 2, col = "blue")
+
+qqnorm(y=bee$Transit.Time)
+qqline(y = bee$Transit.Time)
+#we see that if we would just use a normal distribution, the normal distribution 
+#would understate the extreme positive values of x
+
+#install.packages("MASS")
+library(MASS)
+
+normest = fitdistr(bee$Transit.Time, "normal")
+curve(dnorm(x, normest$estimate[1], normest$estimate[2]), add=TRUE, col='red')
+
+gammaest = fitdistr(bee$Transit.Time, densfun="gamma")
+curve(dgamma(x, gammaest$estimate[1], gammaest$estimate[2]), add=TRUE, col='blue')
+
+legend("topright", cex=0.75, pch=16, 
+       col=c("red", "blue"), legend=c("Normal Dist", "Gamma Dist"))
+
+
+#curve(dgamma(x= BeeTable$Transit.Time, shape=2, scale=1.5), from= 0, to = 20, main = "Gamma Distribution")
+# dgamma(x= bee$Transit.Time, shape=1, scale=1.5)
+#make a slider thingy to adjust the shape and the scale
+
+library("")
+
+#Next, need to make tutorial and teach about what a gamma distribution is and why the normal curve is not a good
+#fit for this data 
+
+#Put onto a shiny package with little button and slider thingies 
+
+#one check box for normal or gamma dist
+#one slider for changing the gamma parameters maybe? <--would need to know how to choose 
+#best parameters...
+
+#somehow need to figure out how to make it interactive 
